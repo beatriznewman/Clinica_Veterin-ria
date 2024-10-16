95957372f7c93c27ce18637520b2e960757a155f
Beatriz Newman <102912868+beatriznewman@users.noreply.github.com>
2024-10-16 20:38:58 -0300

    Update Consulta.php

--- Consulta.php
+++ Consulta.php
@@ -9,7 +9,6 @@ class Consulta extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
     protected $fillable = ['paciente_id', 'user_id', 'animal_id', 'data', 'horario_inicio', 'horario_fim', 'informacao'];
 
     public function animal()
@@ -21,13 +20,6 @@ class Consulta extends Model
     public function paciente()
     {
         return $this->belongsTo(Paciente::class); // Ajuste se o relacionamento for diferente
-=======
-    protected $fillable = ['paciente_id', 'user_id', 'data', 'horario_inicio', 'horario_fim', 'informacao'];
-
-    public function paciente()
-    {
-        return $this->belongsTo(Paciente::class, 'paciente_id');
->>>>>>> 2dd8a3ceedfef734f3419ea03ce856f46b15d01d
     }
 
     public function user()
